e741658cf0bcb94129e417094992c58d2a4690d3
Jamir <championjamir@gmail.com>
2022-12-04 15:06:03 -0500

    commit tree fab0696fa48bfeec2e76dd40ff92aa92d3b8f5ce

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const surprise = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(surprise) {
+     var cards = []
+    for (let i = 0; i < surprise.length; i++) {
+        cards.push(`Thank you, ${surprise[i]}, for the wonderful surprise gift!`) 
+
+    
+}
+
+return cards
+
+}
+function countDown() {
+    
+    for (let countDown = 0; countDown < 11; countDown++) {
+        console.log(countDown)
+    }
+}
